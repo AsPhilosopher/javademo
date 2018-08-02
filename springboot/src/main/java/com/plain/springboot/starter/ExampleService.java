434ccc742cdec0542c47eef352b662a61bc92dec
Lincon <1003349954@qq.com>
2018-08-02 23:36:42 +0800

    SpringBoot Starter

---
+++ springboot/src/main/java/com/plain/springboot/starter/ExampleService.java
@@ -0,0 +1,16 @@
+package com.plain.springboot.starter;
+
+public class ExampleService {
+
+    private String prefix;
+    private String suffix;
+
+    public ExampleService(String prefix, String suffix) {
+        this.prefix = prefix;
+        this.suffix = suffix;
+    }
+
+    public String wrap(String word) {
+        return prefix + word + suffix;
+    }
+}
---
+++ springboot/src/test/java/com/plain/springboot/starter/Application.java
@@ -0,0 +1,25 @@
+package com.plain.springboot.starter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    private ExampleService exampleService;
+
+    @GetMapping("/input")
+    public String input(String word) {
+        return exampleService.wrap(word);
+    }
+
+}
