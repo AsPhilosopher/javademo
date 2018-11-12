fd46611c9ee4bfbd0f5b6100f10190ebf74d37d8
Lincon <1003349954@qq.com>
2018-11-13 00:50:37 +0800

    add:rabbitMq transaction and confirm

--- mq/rabbitmq/src/main/java/com/plain/rabbitmq/RabbitMqDemo.java
+++ mq/rabbitmq/src/main/java/com/plain/rabbitmq/RabbitMqDemo.java
@@ -44,30 +44,7 @@ public class RabbitMqDemo {
 //        channel.queueBind(queueName, myExchange, routeKey); //绑定队列与exchange
 
         channel.basicPublish(myExchange, routeKey, null, messageBodyBytes);
-
-        boolean autoAck = false;
-        channel.basicConsume(queueName, autoAck, myConsumerTag,
-                new DefaultConsumer(channel) {
-                    @Override
-                    public void handleDelivery(String consumerTag,
-                                               Envelope envelope,
-                                               AMQP.BasicProperties properties,
-                                               byte[] body)
-                            throws IOException {
-                        System.out.println("consumerTag: " + consumerTag);
-                        String routingKey = envelope.getRoutingKey();
-                        System.out.println("routingKey: " + routingKey);
-                        String contentType = properties.getContentType();
-                        System.out.println("contentType: " + contentType);
-                        long deliveryTag = envelope.getDeliveryTag();
-                        System.out.println("deliveryTag: " + deliveryTag);
-                        // (process the message components here ...)
-                        System.out.println("body: " + new String(body));
-                        //这里做确认操作，不确认的话，channel关闭则消息又回到ready状态
-                        channel.basicAck(deliveryTag, false);
-                    }
-                });
-        channel.basicCancel(myConsumerTag);
+        basicConsume();
     }
 
     @Test
@@ -99,9 +76,90 @@ public class RabbitMqDemo {
         }
     }
 
+    @Test
+    public void transaction() throws IOException {
+        channel.txSelect();
+        try {
+            channel.basicPublish(myExchange, routeKey, null, messageBodyBytes);
+//            int a = 10 / 0;
+            channel.txCommit();
+        } catch (IOException e) {
+            e.printStackTrace();
+            channel.txRollback();
+        } catch (Exception e) {
+            e.printStackTrace();
+            channel.txRollback();
+        }
+        basicConsume();
+    }
+
+    @Test
+    public void confirm() throws IOException, InterruptedException {
+        channel.confirmSelect();
+        channel.basicPublish(myExchange, routeKey, null, messageBodyBytes);
+        if (channel.waitForConfirms()) {
+            System.out.println("消息发送成功" );
+        }
+        basicConsume();
+
+        int num = 10;
+        for (int i = 0; i < num; i++) {
+            channel.basicPublish(myExchange, routeKey, null, messageBodyBytes);
+        }
+        channel.waitForConfirmsOrDie(); //直到所有信息都发布，只要有一个未确认就会IOException
+        System.out.println("消息全部发送成功");
+        for (int i = 0; i < num; i++) {
+            basicConsume();
+        }
+
+        for (int i = 0; i < num; i++) {
+            channel.basicPublish(myExchange, routeKey, null, messageBodyBytes);
+        }
+        //异步监听确认和未确认的消息
+        channel.addConfirmListener(new ConfirmListener() {
+            @Override
+            public void handleNack(long deliveryTag, boolean multiple) throws IOException {
+                System.out.println("未确认消息，标识：" + deliveryTag);
+            }
+            @Override
+            public void handleAck(long deliveryTag, boolean multiple) throws IOException {
+                System.out.println(String.format("已确认消息，标识：%d，多个消息：%b", deliveryTag, multiple));
+            }
+        });
+        for (int i = 0; i < num; i++) {
+            basicConsume();
+        }
+    }
+
     @After
     public void after() throws IOException, TimeoutException {
         channel.close();
         connection.close();
+    }
+
+    private void basicConsume() throws IOException {
+        boolean autoAck = false;
+        channel.basicConsume(queueName, autoAck, myConsumerTag,
+                new DefaultConsumer(channel) {
+                    @Override
+                    public void handleDelivery(String consumerTag,
+                                               Envelope envelope,
+                                               AMQP.BasicProperties properties,
+                                               byte[] body)
+                            throws IOException {
+                        System.out.println("consumerTag: " + consumerTag);
+                        String routingKey = envelope.getRoutingKey();
+                        System.out.println("routingKey: " + routingKey);
+                        String contentType = properties.getContentType();
+                        System.out.println("contentType: " + contentType);
+                        long deliveryTag = envelope.getDeliveryTag();
+                        System.out.println("deliveryTag: " + deliveryTag);
+                        // (process the message components here ...)
+                        System.out.println("body: " + new String(body));
+                        //这里做确认操作，不确认的话，channel关闭则消息又回到ready状态
+                        channel.basicAck(deliveryTag, false);
+                    }
+                });
+        channel.basicCancel(myConsumerTag);
     }
 }
